9bfaecb38318fc4fbd829db54c1574f93be51407
yeicops02 <158612301+yeicops02@users.noreply.github.com>
2025-05-19 23:20:01 -0600

    Cree 2 variables globales que son contadores, aparte cree la function playRound que imprime en la consola si perdiste o ganaste

--- JS/script.js
+++ JS/script.js
@@ -17,4 +17,34 @@ function getHumanChoice () {
     return entradaUsuario;
 }
 
- 
+//Variables de puntuación de los jugadores
+let humanScore = 0;
+let computerScore = 0;
+
+//Jugar solo 1 ronda
+function playRound (humanChoice, computerChoice) {
+    let eleccionUsuario = humanChoice.toLowerCase();
+
+    if (eleccionUsuario === 'piedra' && computerChoice === 'tijera') {
+        humanScore++;
+        console.log(`¡Ganaste!. ${eleccionUsuario} le gana a la ${computerChoice}`)
+    } else if(eleccionUsuario === 'piedra' && computerChoice === 'papel') {
+        computerScore++;
+        console.log(`¡Perdiste!. ${computerChoice} le gana a la ${eleccionUsuario}`)
+    } else if (eleccionUsuario === computerChoice) {
+        console.log('Hay un empate ')
+    } else if (eleccionUsuario === 'tijera' && computerChoice === 'piedra') {
+        computerScore++;
+        console.log(`¡Perdiste!. ${computerChoice} le gana a la ${eleccionUsuario}`)
+    } else if (eleccionUsuario === 'tijera' && computerChoice === 'papel') {
+        humanScore++;
+        console.log(`¡Ganaste!. ${eleccionUsuario} le gana a la ${computerChoice}`)
+    } else if (eleccionUsuario === 'papel' && computerChoice === 'piedra'){
+        humanScore++;
+        console.log(`¡Ganaste!. ${eleccionUsuario} le gana a la ${computerChoice}`)
+    } else if (eleccionUsuario === 'papel' && computerChoice === 'tijera') {
+        computerScore++;
+        console.log(`¡Perdiste!. ${computerChoice} le gana a la ${eleccionUsuario}. Puntaje`)
+    }
+}
+
